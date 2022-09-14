d492c409a8ed748e76e7de4bb9daadf978f0052a
mathkin-dwarf <dwarf.is.engineer@gmail.com>
2022-09-14 18:49:46 +0900

    Add calling API process.

--- nextjs-project/api/pages/index.tsx
+++ nextjs-project/api/pages/index.tsx
@@ -1,12 +1,35 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
+  const getData = async () => {
+    const response = await fetch(
+      '/nextjs-api/api/data?val1=GetMethodValue1&val2=10000',
+      { method: 'GET' }
+    )
+    const json = await response.json()
+    console.log(json)
+  }
+  const postData = async () => {
+    const headers = {'Content-Type': 'application/json'}
+    const postData = {
+      val1: 'PostMethodValue1',
+      val2: 10001
+    }
+    const response = await fetch(
+      '/nextjs-api/api/data', 
+      { method: 'POST', headers: headers, body: JSON.stringify(postData) }
+    )
+    const json = await response.json()
+    console.log(json)
+  }
   return (
     <div>
       <header>
       </header>
 
       <main>
+        <button onClick={getData}>GET method</button>
+        <button onClick={postData}>POST method</button>
       </main>
 
       <footer>
