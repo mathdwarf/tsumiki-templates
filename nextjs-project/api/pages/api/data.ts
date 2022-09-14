7a68f5771d60bb52a6b4f8a51f4ec48178c7a051
mathkin-dwarf <dwarf.is.engineer@gmail.com>
2022-09-14 18:46:56 +0900

    Add basic API (GET, POST)

--- nextjs-project/api/pages/api/data.ts
+++ nextjs-project/api/pages/api/data.ts
@@ -6,6 +6,21 @@ interface Data {
 }
 
 const dataHandler = ( req: NextApiRequest, res: NextApiResponse<Data> ) => {
-  res.status(200).json({ string: 'string value', number: 9999 })
+  switch (req.method) {
+    case 'GET': {
+      const { val1, val2 } = req.query
+      res.status(200).json({ string: `val1: ${val1}, val2: ${val2}`, number: 0 })
+      break
+    }
+    case 'POST': {
+      const { val1, val2 } = req.body
+      res.status(200).json({ string: `val1: ${val1}, val2: ${val2}`, number: 1 })
+      break
+    }
+    default: {
+      res.status(405).end()
+      break
+    }
+  }
 }
 export default dataHandler
