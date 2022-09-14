5d46a3132cfbd5567e2e950d65bc19bd1abb6e26
mathkin-dwarf <dwarf.is.engineer@gmail.com>
2022-09-14 18:48:06 +0900

    Set path to subdirectory.

--- nextjs-project/api/next.config.js
+++ nextjs-project/api/next.config.js
@@ -2,6 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: "/nextjs-api",
+  basePath: "/nextjs-api",
+  async rewrites() {
+    return [
+      {
+        source: "/nextjs-api/api/:path*",
+        destination: "/api/:path*",
+      },
+      {
+        source: "/nextjs-api/images/:query*",
+        destination: '/_next/images/:query*'
+      },
+      {
+        source: "/nextjs-api/_next/:path*",
+        destination: "/_next/:path*",
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
