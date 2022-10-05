a173910b1a34fcee7cc7b6b459a6e779ce253235
mathkin-dwarf <dwarf.is.engineer@gmail.com>
2022-10-05 18:02:54 +0900

    Add post json API.

--- flask-project/api/app/api/template_api.py
+++ flask-project/api/app/api/template_api.py
@@ -9,6 +9,39 @@ from werkzeug.exceptions import \
 template_api = Blueprint('template_api', __name__, url_prefix='/template')
 CORS(template_api)
 
+@template_api.route("/post-json", methods=["POST"])
+def post_json():
+    logger.info(request)
+
+    response_json = '{}'
+    status_code = 200
+
+    try:
+        if request.method != 'POST':
+            raise MethodNotAllowed('Method is not allowed.')
+
+        json = request.data.decode('utf-8')
+        logger.info(json)
+
+        try:
+            # 
+            # The API functions body is coding here.
+            # 
+            pass
+
+        except Exception as e:
+            logger.error(f'Internal process was failed : {str(e)}')
+            raise InternalServerError('Internal process was failed.')
+
+        response_json = jsonify({'response': 'This is an api response data.'})
+        status_code = 200
+
+    except Exception as e:
+        response_json, status_code = get_exception_status_code(e)
+
+    logger.info('The process with a post file is completed.')
+    return response_json, status_code
+
 @template_api.route("/post-files", methods=["POST"])
 def post_files():
     logger.info(request)
